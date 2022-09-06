66a6adc5936905f4f9bc24aa43b96a6e6d271b74
f̣ộx̣ệ6 <foxe6@protonmail.com>
2022-09-06 10:45:25 +0800

    Update on_download_complete.py

--- resources/aria2/on_download_complete.py
+++ resources/aria2/on_download_complete.py
@@ -6,4 +6,4 @@ import time
 download_id = sys.argv[1]
 num_files = sys.argv[2]
 first_file = sys.argv[3]
-open("aria2_complete.log", "ab").write("{ }{} {} {}\n".format(time.time(), download_id, num_files, first_file).encode())
+open("/content/aria2_complete.log", "ab").write("{ }{} {} {}\n".format(time.time(), download_id, num_files, first_file).encode())
