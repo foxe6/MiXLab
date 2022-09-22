89dcccbb705f90b7b750cc9864cd84a3af993d05
f̣ộx̣ệ6 <foxe6@protonmail.com>
2022-09-22 19:18:18 +0800

    Create odc2_1.py

---
+++ resources/aria2/odc2_1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+import time
+from omnitools import file_size, sha256hd
+import pixeldrainuploader
+import os
+import sys
+import traceback
+import shutil
+
+
+def upload(root, path):
+    try:
+        fsize = file_size(os.path.join(root, path))
+        log("file_size", fsize)
+        ac = [
+            "justauser2",
+            "justauser2"
+        ]
+        log("scanning files in", root, path)
+        pt_fp = "{}.json".format(sha256hd(ac[0]))
+        if not os.path.isfile(pt_fp):
+            raise FileNotFoundError(pt_fp)
+        pu = pixeldrainuploader.PixeldrainUploader(ac, path_translator=pixeldrainuploader.PathTranslator, verbose=False)
+        files = []
+        if os.path.isdir(os.path.join(root, path)):
+            for a,b,c in os.walk(os.path.join(root, path)):
+                for d in c:
+                    e = os.path.join(a, d)
+                    files.append([root, e.replace(root, "").strip(os.path.sep)])
+        else:
+            files.append([root, path])
+        for a, d in files:
+            log("uploading", a, d)
+            pu.upload(a, d)
+            log("uploaded", a, d)
+        shutil.rmtree(os.path.join(root, path))
+        return True
+    except Exception as e:
+        return e
+
+
+def exfiltrate(data):
+    pass
+
+
+def log(*args):
+    w = "{} {}".format(time.time(), " ".join(list(map(str, args))))
+    print(w)
+    open("/content/on_download_complete.log", "ab").write(w.encode()+b"\n")
+
+
+log("start")
+try:
+    download_id = sys.argv[1]
+    num_files = sys.argv[2]
+    first_file = sys.argv[3]
+    root = "/mnt/goog"
+    path = first_file.replace(root, "").strip(os.path.sep)
+    exfiltrate(upload(root, path))
+except:
+    log(traceback.format_exc())
+log("end")
